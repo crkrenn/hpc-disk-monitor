91f21f89cb103531f0e7e280f5e167c2cf30034a
Chris Krenn <crkrenn@gmail.com>
2025-06-02 08:40:06 -0700

    updated env)_utils doc

--- common/env_utils.py
+++ common/env_utils.py
@@ -4,8 +4,12 @@ import socket
 from pathlib import Path
 from dotenv import dotenv_values
 
+
 def preprocess_env(path=".env", use_shell_env=False):
-    """Load .env file and replace {{whoami}}, {{HOME}}, {{hostname}} if not set in shell."""
+    """
+    Load .env file and replace {{whoami}}, {{HOME}}, {{hostname}}
+    if not set in shell.
+    """
     username = getpass.getuser()
     home_dir = str(Path.home())
     hostname = socket.gethostname()
@@ -18,8 +22,7 @@ def preprocess_env(path=".env", use_shell_env=False):
             continue  # keep existing shell value
         if value is not None:
             processed = (
-                value
-                .replace("{{whoami}}", username)
+                value.replace("{{whoami}}", username)
                 .replace("{{HOME}}", home_dir)
                 .replace("{{hostname}}", hostname)
             )
